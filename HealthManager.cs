7664f48abe944c410e9fc96dc6f7fc4e5b16471f
shikjazz <96900337+shikjazz@users.noreply.github.com>
2024-09-08 02:34:44 +0530

    Create HealthManager.cs

---
+++ HealthManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Handle player death (e.g., respawn, game over, etc.)
+        Debug.Log("Player has died.");
+    }
+}
